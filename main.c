2e4439a3cfdbc04253e7e6e03d1d25ab9b27fe75
Giovanni Minoccari <giovanniminoccari@MBP-di-Giovanni.homenet.telecomitalia.it>
2022-05-07 17:50:47 +0200

    feat: initial commit

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MAX_CHAR 40
+
+typedef struct giocatore{
+    char colore;
+    int num;
+    int pos;
+}giocatore;
+
+typedef struct casella{
+    int num;
+    int type;
+    int val;
+    char dom[MAX_CHAR];
+    char risp[MAX_CHAR];
+}casella;
+
+int main() {
+    giocatore *testa=NULL, *coda=NULL;
+    casella table[62];
+    genera_partita(testa, coda, table);
+    gioca_partita(testa, coda, table);
+    return 0;
+}
+genera_partita()
