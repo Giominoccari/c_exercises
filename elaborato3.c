86fdae61f2eb9ac6b4218e84ac3b3098983b3068
Giominoccari <98152727+Giominoccari@users.noreply.github.com>
2022-05-31 22:57:38 +0200

    Update elaborato3.c
    probabilmente la ricerca indicizzata delle cifre partendo dal puntatore num non funziona

--- elaborato3.c
+++ elaborato3.c
@@ -1,13 +1,64 @@
 #include <stdio.h>
 
 void main() {
-	unsigned int numero = ;
+	unsigned int numero = 0x00300001 ;
 	int numzeri;
 	int numuni;
 
 	__asm {
+		XOR EAX, EAX
+		XOR ECX, ECX
+		XOR EDX, EDX
+		XOR EBX, EBX
+		XOR ECX, ECX
+
+		CMP numero[ECX], 0
+		JE start1
+		JNE start0
+
+		start1:MOV EBX, 1
+		MOV EDX, 1
+		XCHG EDX, numuni
+	
+
+		start0: MOV EBX, 0
+		MOV EDX, 1
+		XCHG EDX, numzeri
+		
+
+		XOR EDX, EDX
+
+		uno:CMP EDX, 31
+		JE fine
+		INC EDX
+		CMP numero[ECX], 0
+		JE zeri
+		JNE uni
+
+		zeri:CMP EBX, 0
+		JNE change1
+		INC ECX
+		INC numzeri
+		JMP uno
+		change1: MOV numzeri, 1
+		XOR EBX, EBX
+		INC ECX
+		JMP uno
+
+		uni:CMP EBX, 1
+		JNE change2
+		INC ECX
+		INC numuni
+		JMP uno
+		change2: MOV numuni, 1
+		MOV EBX, 1
+		INC ECX
+		JMP uno
+
+		fine:
+
 
 	}
-	prinf("la sequenza più lunga di bit contigui a 1 è pari a:%d"; numuni);
-	prinf("la sequenza più lunga di bit contigui a 0 è pari a:%d"; numzeri);
+	printf("la sequenza più lunga di bit contigui a 1 è pari a:%d\n", numuni);
+	printf("la sequenza più lunga di bit contigui a 0 è pari a:%d\n", numzeri);
 }
