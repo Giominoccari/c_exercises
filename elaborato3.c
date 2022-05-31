30249c4fa2970699e712ac033d0278f8dcc457ad
Giominoccari <98152727+Giominoccari@users.noreply.github.com>
2022-05-31 17:05:39 +0200

    Create elaborato3.c

---
+++ elaborato3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main() {
+	unsigned int numero = ;
+	int numzeri;
+	int numuni;
+
+	__asm {
+
+	}
+	prinf("la sequenza più lunga di bit contigui a 1 è pari a:%d"; numuni);
+	prinf("la sequenza più lunga di bit contigui a 0 è pari a:%d"; numzeri);
+}
