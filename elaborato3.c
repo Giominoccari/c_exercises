1d3eb38fb4c81b0def66598790eb08b9aff39e04
Giominoccari <98152727+Giominoccari@users.noreply.github.com>
2022-06-03 17:50:05 +0200

    Update elaborato3.c

--- elaborato3.c
+++ elaborato3.c
@@ -1,57 +1,61 @@
 #include <stdio.h>
 
 void main() {
-	unsigned int numero = ;
+	unsigned int numero = 0x00000000 ;
 	int numzeri;
 	int numuni;
 
 	__asm {
-		MOV EAX, numero
+		XOR EAX, EAX
 		XOR EBX, EBX
 		XOR EDX, EDX
 		XOR ECX, ECX
 
 
-		uno:
+		uno :
 		CMP EAX, 32
-		JE fine
-		SAR numero, 1
-		JNC zeri
-		JC uni
+			INC EAX
+			JE fine
+			SAR numero, 1
+			JNC zeri
+			JC uni
 
-		zeri: 
+			zeri :
 		CMP EBX, 0
-		JNE nonprimo
-		MOV EDX, 1
-		MOV EBX, 2
-		nonprimo: CMP EBX, 1
-		JNE nuovaseq1
-		INC EDX
-		JMP uno
-		nuovaseq1: CMP numuni, EDX
-		JG minore
-		MOV numzeri, EDX
-		minore:MOV EDX, 1
-		JMP uno
+			JNE nonprimo
+			MOV EDX, 1
+			MOV EBX, 2
+			JMP uno
+			nonprimo : CMP EBX, 2
+			JNE nuovaseq1
+			INC EDX
+			JMP uno
+			nuovaseq1 : CMP numzeri, EDX
+			MOV EBX, 2
+			JG minore
+			MOV numzeri, EDX
+			minore : MOV EDX, 1
+			JMP uno
 
-		uni:
+			uni :
 		CMP EBX, 0
-		JNE nonprimo
-		MOV ECX, 1
-		MOV EBX, 2
-		nonprimo: CMP EBX,2
-		JNE nuovaseq2
-		INC ECX
-		JMP uno
-		nuovaseq2: CMP numzeri, ECX
-		JG minore2
-		MOV numzeri, ECX
-		minore2: MOV ECX, 1
-		JMP uno
+			JNE nonprimo2
+			MOV ECX, 1
+			MOV EBX, 1
+			JMP uno
+			nonprimo2 : CMP EBX, 1
+			JNE nuovaseq2
+			INC ECX
+			JMP uno
+			nuovaseq2 : CMP numuni, ECX
+			MOV EBX, 1
+			JG minore2
+			MOV numzeri, EDX
+			minore2 : MOV ECX, 1
+			JMP uno
 
-		fine:
+			fine :
 
 	}
 	printf("la sequenza piu lunga di bit contigui a 0 e pari a:%d\n", numzeri);
 	printf("la sequenza piu lunga di bit contigui a 1 e pari a:%d\n", numuni);
-}
