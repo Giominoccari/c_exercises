ff3db708c46180d351c7b955b891bf6eadc9a6ca
Giominoccari <98152727+Giominoccari@users.noreply.github.com>
2022-06-07 10:15:58 +0200

    Update elaborato3.c
    algoritmo completo

--- elaborato3.c
+++ elaborato3.c
@@ -1,27 +1,28 @@
 #include <stdio.h>
 
 void main() {
-	unsigned int numero = 0x00000000 ;
+	unsigned int numero = 0x00001000;
 	int numzeri;
 	int numuni;
 
 	__asm {
-		XOR EAX, EAX
+		MOV EAX, 32
 		XOR EBX, EBX
 		XOR EDX, EDX
 		XOR ECX, ECX
-
+		MOV numuni, 0
+		MOV numzeri, 0
 
 		uno :
-		CMP EAX, 32
-			INC EAX
+		    	CMP EAX, 0
 			JE fine
 			SAR numero, 1
+			DEC EAX
 			JNC zeri
 			JC uni
 
-			zeri :
-		CMP EBX, 0
+		zeri :
+		    	CMP EBX, 0
 			JNE nonprimo
 			MOV EDX, 1
 			MOV EBX, 2
@@ -31,14 +32,18 @@ void main() {
 			INC EDX
 			JMP uno
 			nuovaseq1 : CMP numzeri, EDX
-			MOV EBX, 2
 			JG minore
+			MOV EBX, 2
 			MOV numzeri, EDX
+			MOV EDX, 1
+			JMP uno
 			minore : MOV EDX, 1
+			MOV EBX, 1
 			JMP uno
 
-			uni :
-		CMP EBX, 0
+		uni :
+			MOV numuni, 1
+		    	CMP EBX, 0
 			JNE nonprimo2
 			MOV ECX, 1
 			MOV EBX, 1
@@ -48,14 +53,26 @@ void main() {
 			INC ECX
 			JMP uno
 			nuovaseq2 : CMP numuni, ECX
-			MOV EBX, 1
 			JG minore2
-			MOV numzeri, EDX
+			MOV EBX, 1
+			MOV numuni, ECX
+			MOV ECX, 1
+			JMP uno
 			minore2 : MOV ECX, 1
+			MOV EBX, 1
 			JMP uno
 
-			fine :
+		fine :
+			CMP numuni, ECX
+			JG stop
+			MOV numuni, ECX
+			stop:CMP numzeri, EDX
+			JG stop1
+			MOV numzeri, EDX
+			stop1:
+
 
 	}
 	printf("la sequenza piu lunga di bit contigui a 0 e pari a:%d\n", numzeri);
 	printf("la sequenza piu lunga di bit contigui a 1 e pari a:%d\n", numuni);
+}
